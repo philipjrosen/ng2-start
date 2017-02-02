4147ef99e2e71f855c9502be2536c902ce06ddaa
Philip Rosen <philipjrosen@gmail.com>
2017-02-01 20:55:42 -0800

    Angular 2 Basics

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.message = 'Hello!';
+        this.users = [
+            { id: 25, name: 'Chris', username: 'sevilayha' },
+            { id: 26, name: 'Nick', username: 'whatnicktweets' },
+            { id: 27, name: 'Holly', username: 'hollylawly' }
+        ];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n        <div class=\"jumbotron\">\n            <h1>Welcome to an Angular 2 App!</h1>\n        </div>\n    ",
-        styles: ["\n        .jumbotron { box-shadow: 0 2px 0 rgba(0,0,0,0) }\n    "]
+        template: "\n    <header>\n      <nav class=\"navbar navbar-inverse\">\n        <div class=\"navbar-header\">\n          <a href=\"/\" class=\"navbar-brand\">My Angular 2 App!</a>\n        </div>\n      </nav>\n    </header>\n    <main>\n      <div class=\"jumbotron\">\n        <h1>Welcome to Our App!</h1>\n        <p>{{ message }}</p>\n      </div>\n      <div *ngIf=\"users\">\n      <div *ngFor=\"let user of users\">\n        {{ user.name }} ({{ user.username }})\n      </div>\n      </div>\n    </main>\n    <footer class=\"text-center\">\n      Copyright &copy; 2016\n    </footer>\n  ",
+        styles: ["\n    .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }\n  "]
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
